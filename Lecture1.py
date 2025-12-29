c992772fefabd2d1b5e6513ee221eeea9cee7288
AiWithAlihusnain <alihusnin499@gmail.com>
2025-12-29 12:39:57 +0500

    Add files via upload

---
+++ Lecture1.py
@@ -0,0 +1,18 @@
+#print("to print first 4 numbers product")
+#print(1*2*3*4)
+# Variables 
+name = "Ali Husnain" # string
+Age = "19" # integer
+height = 5.8 # float
+print (name + Age)
+print (Age + Age)
+
+
+
+
+
+# Input From User 
+
+
+name = input("Enter your name: ")
+print ("welcome Bro",name)
